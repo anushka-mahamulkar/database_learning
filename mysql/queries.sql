7e1e0b8a85e3173c2450fc3c8f8793e903695422
Anushka Mahamulkar <anushka.mahamulkar@gmail.com>
2021-08-22 00:18:06 +0530

    database

---
+++ mysql/queries.sql
@@ -0,0 +1,24 @@
+ \c testdb;  = To connect to testdb (test databse).
+     DROP DATABASE testdb; = To remove testdb
+
+
+
+     \d = drops down all the tables in attached database
+     \d tablename = To describe all the table created in that name
+
+
+
+     DROP TABLE table_name; = To remove a table definition and all associated 
+
+
+    A schema is a named collection of tables.
+     CREATE SCHEMA name; = To create table in schema.
+
+     DROP SCHEMA myschema; = To drop the schema
+
+
+    INSERT INTO statement allows one to insert new rows into a table.
+     SELECT * FROM TABLE_NAME; = To fetch the data from all the fields in the table.
+
+Date format = yyyy-mm-dd.
+Select upper (FIRST_NAME) from table name; = To fetch first_name from table name in upper case.   
